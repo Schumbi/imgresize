b4bfd6727079bd314b087f55b11262aa5d66a6ef
Schumbi <dev@schumbi.de>
2023-01-05 21:27:28 +0100

    some kind of working version

--- Program.cs
+++ Program.cs
@@ -1,8 +1,59 @@
-﻿namespace imageResizer;
-class Program
+﻿namespace ImageResizer
 {
-    static void Main(string[] args)
+    using System;
+
+    using SixLabors.ImageSharp;
+    using SixLabors.ImageSharp.Formats.Jpeg;
+    using SixLabors.ImageSharp.Processing;
+    using SixLabors.ImageSharp.PixelFormats;
+    using SixLabors.ImageSharp.Formats;
+
+    class Program
     {
-        Console.WriteLine("Hello, World!");
+        static readonly string sourceDirectory = "images";
+
+        static readonly string destinatinDirectory = "resized";
+
+        static readonly string movedDirectory = "original";
+
+
+
+        static void Main(string[] args)
+        {
+            var watcher = new FileSystemWatcher(sourceDirectory)
+            {
+                EnableRaisingEvents = true,
+                Filters = { "*.jpg", "*.JPG" },
+                IncludeSubdirectories = true,
+            };
+            watcher.Created += OnFileCreated;
+
+            while(true)
+            {
+                Thread.Sleep(1000);
+                Console.Write(".");
+            }
+        }
+
+        static void OnFileCreated(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine(e.FullPath);
+            using (var stream = File.OpenRead(e.FullPath))
+            {
+
+                var image = Image.Load<Rgba32>(stream, out IImageFormat format);
+
+                int newWidth = image.Width / 2;
+                int newHeight = (int)(image.Height * ((float)newWidth / image.Width));
+
+                image.Mutate(x => x.Resize(newWidth, newHeight));
+
+                using var writeStream = File.OpenWrite(Path.Combine(destinatinDirectory, e.Name));
+
+                image.Save(writeStream, new JpegEncoder { ColorType = JpegColorType.Rgb, Quality = 85 });
+            }
+
+            File.Move(e.FullPath, Path.Combine(movedDirectory, e.Name));
+        }
     }
 }
