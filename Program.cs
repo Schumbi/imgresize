4c63f61c26b7d1ab4545fddb16a3e6320aeb1f80
Ralf Warmuth <r.warmuth@schumbi.de>
2023-01-18 08:58:00 +0100

    Startet with the implementation of some arguments

--- Program.cs
+++ Program.cs
@@ -1,25 +1,59 @@
 ﻿namespace ImageResizer
 {
     using System.Reactive.Linq;
+    using System.CommandLine;
 
     using static LanguageExt.Prelude;
 
     class Program
     {
-        static async Task Main(string[] args)
+        static int Main(string[] args)
+        {
+            var sourceArg = new System.CommandLine.Option<DirectoryInfo?>(
+                name: "--source",
+                description: "The directory with source files!"
+            );
+            sourceArg.Arity = ArgumentArity.ExactlyOne;
+
+            var destArg = new System.CommandLine.Option<DirectoryInfo?>(
+                name:"--destination",
+                getDefaultValue: () => new DirectoryInfo( $"{Path.GetTempPath()}{Path.DirectorySeparatorChar}imgResizer_originals" ),
+                description: "The directory the files should be moved to."
+            );
+
+            var moveArg = new System.CommandLine.Option<DirectoryInfo?>(
+                name: "--move",
+                getDefaultValue: () => new DirectoryInfo( $"{Path.GetTempPath()}{Path.DirectorySeparatorChar}imgResizer_resized" ),
+                description: "The directory for the resized images!"
+            );
+            var rootCommand = new RootCommand("Resizes JPG-Images and moves images.")
+            {
+                sourceArg,
+                destArg,
+                moveArg
+            };
+
+            return rootCommand.InvokeAsync(args).Result;
+        }
+
+        internal static async Task Resize(DirectoryInfo source, DirectoryInfo destination, DirectoryInfo move)
         {
             var opts = new Options()
             {
-                DestinationDirectory = "original",
-                MovedDirectory = "resized",
-                SourceDirectory = "images",
+                DestinationDirectory = destination.FullName,
+                MovedDirectory = move.FullName,
+                SourceDirectory = source.FullName,
                 Width = 100,
                 Height = 100,
                 KeepAspectRatio = true,
                 MaxConcurrent = 4,
                 CheckDelay = 500 * ms,
             };
+            await ImgResize(opts);
+        }
 
+        static async Task ImgResize(Options opts)
+        {
             CancellationTokenSource cts = new();
             Console.CancelKeyPress += (o, e) =>
             {
