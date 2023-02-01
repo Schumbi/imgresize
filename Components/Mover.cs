ac245d4c051ac1830c09b328e96a9a180fb2cbe2
Schumbi <dev@schumbi.de>
2023-02-02 00:15:52 +0100

    Enhance mover -> To TEST!

--- Components/Mover.cs
+++ Components/Mover.cs
@@ -1,14 +1,7 @@
 ﻿namespace ImageResizer.Components
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-
-    using ImageResizer.Types;
-
-    using LanguageExt;
+    using Types;
+    using Extensions;
 
     public static class Mover
     {
@@ -18,15 +11,18 @@
         /// <param name="source">Source file.</param>
         /// <param name="destination">Destination file.</param>
         /// <returns>Success.</returns>
-        public static bool Move(FilePath source, FilePath destination)
-        {
-            if(source.FileExists && !destination.FileExists)
-            {
-                File.Move(source.Value, destination.Value);
-                return true;
-            }
-            return false;
-        }
+        public static bool Move(FilePath source, FilePath destination) => source
+            .DoIf<FilePath, bool>(
+                s => s.FileExists,
+                src => GuardDestination(destination)
+                    .Match(
+                        Some: fp =>
+                        {
+                            File.Move(source.Value, destination.Value);
+                            return true;
+                        },
+                        None: () => false))
+            .Match(b => b, () => false);
 
         /// <summary>
         /// Move file to directory.
@@ -44,14 +40,34 @@
             return false;
         }
 
-        public static Option<FilePath> GuardDestination(FilePath original, int count = 0)
+        /// <summary>
+        /// Searches for a new filename if file exists.
+        /// </summary>
+        /// <param name="original">Original file path.</param>
+        /// <param name="count">Counter to increase.</param>
+        /// <param name="error">Error.</param>
+        /// <returns></returns>
+        public static LanguageExt.Option<FilePath> GuardDestination(FilePath original, int count = 0, bool error = false)
         {
             if(!original.FileExists)
                 return original;
 
-            var fname = Path.GetFileNameWithoutExtension(original.Value);
-            return Option<FilePath>.None;
+            if (error)
+                return LanguageExt.Option<FilePath>.None;
 
+            // next count
+            count += 1;
+
+            var nextFilePath = original.Bind(o => FilePath
+                .Combine(
+                    original.Directory.Match(p => p, new DirectoryPath(string.Empty)),
+                    $"{original.Name}_{count}",
+                    original.Extension.Match(e => e, string.Empty))
+                .IfNone(FilePath.Empty));
+
+            error = nextFilePath.Directory.IsNone || nextFilePath.Extension.IsNone || nextFilePath == FilePath.Empty();
+
+            return GuardDestination(nextFilePath, count, error);
         }
     }
 }
