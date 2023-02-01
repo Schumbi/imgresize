8828d150f6ee5d4b3ee23a8fd6e128adb05c6da5
Schumbi <dev@schumbi.de>
2023-02-01 22:53:57 +0100

    Started to make the move operation more resilient.

---
+++ Components/Mover.cs
@@ -0,0 +1,57 @@
+﻿namespace ImageResizer.Components
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using ImageResizer.Types;
+
+    using LanguageExt;
+
+    public static class Mover
+    {
+        /// <summary>
+        /// Move a file to a file location.
+        /// </summary>
+        /// <param name="source">Source file.</param>
+        /// <param name="destination">Destination file.</param>
+        /// <returns>Success.</returns>
+        public static bool Move(FilePath source, FilePath destination)
+        {
+            if(source.FileExists && !destination.FileExists)
+            {
+                File.Move(source.Value, destination.Value);
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Move file to directory.
+        /// </summary>
+        /// <param name="source">Source file.</param>
+        /// <param name="destination">Destination directory.</param>
+        /// <returns>Success.</returns>
+        public static bool Move(FilePath source, DirectoryPath destination)
+        {
+            if (source.FileExists && destination.DirectoryExists)
+            {
+                var fileDestination = new FilePath(Path.Combine(destination.Value, Path.GetFileName(source.Value)));
+                return Move(source, fileDestination);
+            }
+            return false;
+        }
+
+        public static Option<FilePath> GuardDestination(FilePath original, int count = 0)
+        {
+            if(!original.FileExists)
+                return original;
+
+            var fname = Path.GetFileNameWithoutExtension(original.Value);
+            return Option<FilePath>.None;
+
+        }
+    }
+}
---
+++ Types/DirectoryPath.cs
@@ -0,0 +1,15 @@
+﻿namespace ImageResizer.Types
+{
+    using LanguageExt;
+
+    using static LanguageExt.Prelude;
+
+    public class DirectoryPath : NewType<DirectoryPath, string>
+    {
+        public DirectoryPath(string value) : base(value)
+        {
+        }
+
+        public bool DirectoryExists => Directory.Exists(Value);
+    }
+}
