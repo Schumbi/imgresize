7b7c4f29a7ec386a16b90d3e41789511b85c23ef
Schumbi <dev@schumbi.de>
2023-04-27 22:52:16 +0200

    Add some file watcher

---
+++ Components/Watcher.cs
@@ -0,0 +1,48 @@
+﻿namespace ImageResizer.Components
+{
+    using System;
+    using System.IO;
+    using System.Threading.Tasks;
+
+    using LanguageExt;
+    using ImageResizer.Types;
+
+    internal class Watcher
+    {
+        public static Task RunWatcher(
+            DirectoryInfo watchedDir,
+            Option<Func<FilePath, Unit>> created,
+            Option<Func<FilePath, Unit>> deleted,
+            Option<Func<Exception, Unit>> errorHandler,
+            Option<Func<NotifyFilters>> notifyFilters,
+            Option<Func<string>> fileFilter,
+            bool recursive,
+            CancellationToken cancellationToken)
+
+             => Task.Run(async () =>
+             {
+                 //watcher.NotifyFilter = NotifyFilters.Size
+                 //            | NotifyFilters.FileName
+                 //            | NotifyFilters.LastWrite;
+
+                 using var watcher = new FileSystemWatcher(watchedDir.FullName);
+
+                 created.IfSome(f => watcher.Created += (object sender, FileSystemEventArgs e) => f.Invoke(FilePath.Create(e.FullPath)));
+
+                 deleted.IfSome(f => watcher.Deleted += (object sender, FileSystemEventArgs e) => f.Invoke(FilePath.Create(e.FullPath)));
+
+                 errorHandler.IfSome(f => watcher.Error += (object sender, ErrorEventArgs e) => f.Invoke(e.GetException()));
+
+                 notifyFilters.IfSome(f => watcher.NotifyFilter = f.Invoke());
+
+                 fileFilter.IfSome(f => watcher.Filter = f.Invoke());
+
+                 watcher.IncludeSubdirectories = recursive;
+                 watcher.EnableRaisingEvents = true;
+
+                 await Task.Delay(-1);
+
+             }, 
+                 cancellationToken);
+    }
+}
